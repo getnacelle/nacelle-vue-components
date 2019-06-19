6d7217bba2fe81847a30bafbe402c1ef6440fe3a
Levi Whalen <levi@leviwhalen.com>
2019-06-19 15:00:00 -0700

    rebuilt library

--- dist/index.js
+++ dist/index.js
@@ -6,15 +6,17 @@
 
 import AddToCartButton from './components/AddToCartButton.vue'
 import ProductPrice from './components/ProductPrice.vue'
+import ProductTitle from './components/ProductTitle.vue'
 import Spinner from './components/Spinner.vue'
 
 // Export components individually
-export { AddToCartButton, ProductPrice, Spinner }
+export { AddToCartButton, ProductPrice, ProductTitle, Spinner }
 
 // What should happen if the user installs the library as a plugin
 function install(Vue) {
   Vue.component('AddToCartButton', AddToCartButton)
   Vue.component('ProductPrice', ProductPrice)
+  Vue.component('ProductTitle', ProductTitle)
   Vue.component('Spinner', Spinner)
 }
 
