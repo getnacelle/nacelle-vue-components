08e3bdb378ea73451022934083441181db30ae69
Levi Whalen <levi@leviwhalen.com>
2019-06-24 14:23:31 -0700

    updated tests

--- tests/unit/productPrice.spec.js
+++ tests/unit/productPrice.spec.js
@@ -4,13 +4,13 @@ import ProductPrice from '@/components/ProductPrice'
 describe('ProductPrice.vue', () => {
   it('renders the price with a dollar sign', async () => {
     const wrapper = shallowMount(ProductPrice, { propsData: { price: '25' } })
-    expect(wrapper.html()).toBe('<div>$25 </div>')
+    expect(wrapper.html()).toBe('<div><p>$25 </p></div>')
   })
 
   it('renders the currency code', async () => {
     const wrapper = shallowMount(ProductPrice, {
       propsData: { price: '25', 'show-currency-code': true }
     })
-    expect(wrapper.html()).toBe('<div>$25 USD</div>')
+    expect(wrapper.html()).toBe('<div><p>$25 USD</p></div>')
   })
 })
