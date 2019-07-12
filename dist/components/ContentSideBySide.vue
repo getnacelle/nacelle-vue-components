7180970914ab36be7d573a3f089b78380b06246e
Andrew Laskey <andrew.laskey@gmail.com>
2019-07-12 11:47:46 -0700

    side by side css update

--- dist/components/ContentSideBySide.vue
+++ dist/components/ContentSideBySide.vue
@@ -1,5 +1,5 @@
 <template>
-  <section class="sbs">
+  <section class="sbs sbs-target">
     <div class="columns" :class="columnClasses">
       <div class="column is-half sbs-image">
         <img :src="imageUrl" alt="" />
@@ -82,7 +82,7 @@ export default {
 }
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
 .sbs .columns {
   margin-top: 0;
 
