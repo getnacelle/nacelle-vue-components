6066836301fb66941fc9b9973fa2b8a66c59a5b5
Levi Whalen <levi@leviwhalen.com>
2019-08-01 09:41:48 -0700

    updated add to cart button to watch variant change

--- src/components/ProductAddToCartButton.vue
+++ src/components/ProductAddToCartButton.vue
@@ -69,8 +69,8 @@ export default {
   },
 
   watch: {
-    confirmedSelection(value) {
-      if (value == true) {
+    variant() {
+      if (this.confirmedSelection == true) {
         this.addToCart()
       }
     }
