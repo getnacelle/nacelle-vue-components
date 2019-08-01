29da5551376e547ccbf42e8f5828fa5d2735788a
Levi Whalen <levi@leviwhalen.com>
2019-08-01 10:35:16 -0700

    updated test

--- tests/unit/productOptions.spec.js
+++ tests/unit/productOptions.spec.js
@@ -1,8 +1,10 @@
 import { mount } from '@vue/test-utils'
+import store from '../../src/store/store'
 import ProductOptions from '@/components/ProductOptions'
 
 describe('ProductOptionSwatch.vue', () => {
   const wrapper = mount(ProductOptions, {
+    store,
     propsData: {
       options: [
         {
