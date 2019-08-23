9be6f2968a783bdc2b8ac8102ce479850b47523c
Andrew Laskey <andrew.laskey@gmail.com>
2019-08-23 16:10:22 -0700

    Add BlogArticleHeader

---
+++ src/components/BlogArticleHeader.vue
@@ -0,0 +1,66 @@
+<template>
+  <header>
+    <slot name="tags" :tags="tags">
+      <h5 v-if="hasTags" class="article-tags">
+        {{ tags.join(', ') }}
+      </h5>
+    </slot>
+    <slot name="title" :title="title">
+      <h1 class="article-title title is-3">
+        {{ title }}
+      </h1>
+    </slot>
+    <slot name="author" :author="author">
+      <p v-if="author.length > 0" class="article-author">
+        {{ author }}
+      </p>
+    </slot>
+    <slot name="date" :date="absoluteDate">
+      <p class="article-published">
+        Published on {{absoluteDate}}
+      </p>
+    </slot>
+    <slot></slot>
+  </header>
+</template>
+
+<script>
+import dayjs from 'dayjs'
+
+export default {
+  props: {
+    title: {
+      type: String,
+      default: '',
+    },
+    author: {
+      type: String,
+      default: ''
+    },
+    tags: {
+      type: Array,
+      default: () => ([])
+    },
+    publishDate: {
+      type: String,
+      default: ''
+    },
+    dateFormatString: {
+      type: String,
+      default: 'MMMM D, YYYY'
+    }
+  },
+  computed: {
+    hasTags() {
+      return (this.tags && this.tags.length > 0)
+    },
+    absoluteDate() {
+      return dayjs(this.publishDate).format(this.dateFormatString)
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
