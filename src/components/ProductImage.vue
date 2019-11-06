51416ff991f2c0554eceab3674c8c8491b7dcb4e
Nick Richmond <nwrichmond@turnt.tech>
2019-11-06 15:52:51 -0500

    Set width param in src query from container width
    This speeds up page load by reducing image size

--- src/components/ProductImage.vue
+++ src/components/ProductImage.vue
@@ -1,18 +1,13 @@
 <template>
-  <div 
+  <div
     v-observe-visibility="{
       callback: visibilityChanged,
       once: true,
     }"
     class="product-image nacelle"
+    ref="img-card"
   >
-    <img
-      v-if="visibility"
-      :src="source"
-      :alt="alt"
-      :width="width"
-      ref="product-image"
-    />
+    <img v-if="visibility" :src="sourceOptimized" :alt="alt" :width="width" ref="product-image" />
   </div>
 </template>
 
@@ -20,6 +15,16 @@
 import Vue from 'vue'
 import VueObserveVisibility from 'vue-observe-visibility'
 Vue.use(VueObserveVisibility)
+
+function fromShopifyCDN(url) {
+  return url.split('.com')[0] === 'https://cdn.shopify'
+}
+
+function fromMagentoCDN(url) {
+  // Note that not all Magento stores use images from the Magento CDN
+  const [, str1, str2, str3] = url.split('://')[1].split('/')
+  return str1.concat('/', str2, '/', str3) === 'media/catalog/product'
+}
 
 export default {
   props: {
@@ -38,11 +43,16 @@ export default {
     observeVisibility: {
       type: Boolean,
       default: true
+    },
+    optimized: {
+      type: Boolean,
+      default: true
     }
   },
   data() {
     return {
-      visible: false
+      visible: false,
+      offsetWidth: null
     }
   },
   computed: {
@@ -52,20 +62,32 @@ export default {
       }
 
       return true
+    },
+    sourceOptimized() {
+      if (this.optimized) {
+        if (fromShopifyCDN(this.source)) {
+          return this.source
+            .split('&width=')[0]
+            .concat(`&width=${this.offsetWidth}`)
+        }
+      } else return this.source
     }
   },
   methods: {
     visibilityChanged(isVisible, entry) {
       this.visible = isVisible
     }
+  },
+  mounted() {
+    if (process.client) {
+      this.offsetWidth = this.$refs['img-card'].offsetWidth
+    }
+    // let vm = this
+    // this.$refs['product-image'].addEventListener('load', e => {
+    //   console.log(e)
+    //   vm.$emit('image-loaded')
+    // })
   }
-  // mounted() {
-  //   let vm = this
-  //   this.$refs['product-image'].addEventListener('load', e => {
-  //     console.log(e)
-  //     vm.$emit('image-loaded')
-  //   })
-  // }
 }
 </script>
 
