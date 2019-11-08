9cf8d65375d0ee40c32c47b4df0790290389836e
Nick Richmond <nwrichmond@turnt.tech>
2019-11-08 15:47:20 -0500

    Add unit tests for ProductImage
    Verifies that the size & format of the image are optimized

--- tests/unit/productImage.spec.js
+++ tests/unit/productImage.spec.js
@@ -2,7 +2,7 @@ import { shallowMount } from '@vue/test-utils'
 import ProductImage from '@/components/ProductImage'
 
 describe('ProductImage/vue', () => {
-  it('renders the image', async () => {
+  it('renders an image', async () => {
     const wrapper = shallowMount(ProductImage, {
       propsData: {
         source:
@@ -13,6 +13,50 @@ describe('ProductImage/vue', () => {
     wrapper.setData({ visible: true })
     expect(wrapper.html()).toBe(
       '<div class="product-image nacelle"><img src="https://www.sparkawards.com/wp-content/uploads/2011/05/Product_Lg_Type-1024x791.jpg" alt="chair"></div>'
+    )
+  })
+  
+  it('optimizes the format of a Shopify image', async () => {
+    const wrapper = shallowMount(ProductImage, {
+      propsData: {
+        source:
+          'https://cdn.shopify.com/s/files/1/0094/4098/5124/products/68.png?v=1563167567&width=750',
+        alt: 'pillow',
+        optimizedSize: false
+      }
+    })
+    wrapper.setData({ visible: true })
+    expect(wrapper.html()).toBe(
+      '<div class="product-image nacelle"><img src="https://cdn.shopify.com/s/files/1/0094/4098/5124/products/68.png?v=1563167567&amp;width=750&amp;format=pjpg" alt="pillow"></div>'
+    )
+  })
+  
+  it('optimizes the size of a Shopify image', async () => {
+    const wrapper = shallowMount(ProductImage, {
+      propsData: {
+        source:
+          'https://cdn.shopify.com/s/files/1/0094/4098/5124/products/68.png?v=1563167567&width=750',
+        alt: 'pillow',
+        optimizedFormat: false
+      }
+    })
+    wrapper.setData({ visible: true, offsetWidth: 450 })
+    expect(wrapper.html()).toBe(
+      '<div class="product-image nacelle"><img src="https://cdn.shopify.com/s/files/1/0094/4098/5124/products/68.png?v=1563167567&amp;width=450" alt="pillow"></div>'
+    )
+  })
+
+  it('optimizes both the size and the format of a Shopify image', async () => {
+    const wrapper = shallowMount(ProductImage, {
+      propsData: {
+        source:
+          'https://cdn.shopify.com/s/files/1/0094/4098/5124/products/68.png?v=1563167567&width=750',
+        alt: 'pillow',
+      }
+    })
+    wrapper.setData({ visible: true, offsetWidth: 450 })
+    expect(wrapper.html()).toBe(
+      '<div class="product-image nacelle"><img src="https://cdn.shopify.com/s/files/1/0094/4098/5124/products/68.png?v=1563167567&amp;width=450&amp;format=pjpg" alt="pillow"></div>'
     )
   })
 })
