857e63160dbd34546ede498f39dbdff3486dfce9
Nick Richmond <nwrichmond@turnt.tech>
2019-11-11 17:51:12 -0500

    Update test to accomodate image opt mixin changes

--- tests/unit/productImage.spec.js
+++ tests/unit/productImage.spec.js
@@ -7,40 +7,43 @@ describe('ProductImage/vue', () => {
       propsData: {
         source:
           'https://www.sparkawards.com/wp-content/uploads/2011/05/Product_Lg_Type-1024x791.jpg',
-        alt: 'chair'
-      }
+        alt: 'chair',
+      },
     })
     wrapper.setData({ visible: true })
     expect(wrapper.html()).toBe(
       '<div class="product-image nacelle"><img src="https://www.sparkawards.com/wp-content/uploads/2011/05/Product_Lg_Type-1024x791.jpg" alt="chair"></div>'
     )
   })
-  
+
   it('optimizes the format of a Shopify image', async () => {
     const wrapper = shallowMount(ProductImage, {
       propsData: {
         source:
           'https://cdn.shopify.com/s/files/1/0094/4098/5124/products/68.png?v=1563167567&width=750',
         alt: 'pillow',
-        optimizedSize: false
-      }
+        resize: false,
+      },
     })
     wrapper.setData({ visible: true })
     expect(wrapper.html()).toBe(
-      '<div class="product-image nacelle"><img src="https://cdn.shopify.com/s/files/1/0094/4098/5124/products/68.png?v=1563167567&amp;width=750&amp;format=pjpg" alt="pillow"></div>'
+      '<div class="product-image nacelle"><img src="https://cdn.shopify.com/s/files/1/0094/4098/5124/products/68.png?v=1563167567&amp;width=750&amp;format=webp" alt="pillow"></div>'
     )
   })
-  
+
   it('optimizes the size of a Shopify image', async () => {
     const wrapper = shallowMount(ProductImage, {
       propsData: {
         source:
           'https://cdn.shopify.com/s/files/1/0094/4098/5124/products/68.png?v=1563167567&width=750',
         alt: 'pillow',
-        optimizedFormat: false
-      }
+        reformat: false,
+      },
     })
-    wrapper.setData({ visible: true, containerWidth: 450 })
+    wrapper.setData({
+      visible: true,
+      containerWidth: 450,
+    })
     expect(wrapper.html()).toBe(
       '<div class="product-image nacelle"><img src="https://cdn.shopify.com/s/files/1/0094/4098/5124/products/68.png?v=1563167567&amp;width=450" alt="pillow"></div>'
     )
@@ -52,11 +55,14 @@ describe('ProductImage/vue', () => {
         source:
           'https://cdn.shopify.com/s/files/1/0094/4098/5124/products/68.png?v=1563167567&width=750',
         alt: 'pillow',
-      }
+      },
     })
-    wrapper.setData({ visible: true, containerWidth: 450 })
+    wrapper.setData({
+      visible: true,
+      containerWidth: 450,
+    })
     expect(wrapper.html()).toBe(
-      '<div class="product-image nacelle"><img src="https://cdn.shopify.com/s/files/1/0094/4098/5124/products/68.png?v=1563167567&amp;width=450&amp;format=pjpg" alt="pillow"></div>'
+      '<div class="product-image nacelle"><img src="https://cdn.shopify.com/s/files/1/0094/4098/5124/products/68.png?v=1563167567&amp;width=450&amp;format=webp" alt="pillow"></div>'
     )
   })
 })
