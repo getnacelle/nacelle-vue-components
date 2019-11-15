ea4f88988007de7dbe9957403d3757fe16e92dc9
Andrew Laskey <andrew.laskey@gmail.com>
2019-11-15 15:15:49 -0800

    update search results source and stories

--- src/stories/searchPage.stories.js
+++ src/stories/searchPage.stories.js
@@ -13,14 +13,13 @@ import RefinementFilters from '../components/RefinementFilters'
 import SearchResults from '../components/SearchResults'
 import SearchNoResults from '../components/SearchNoResults'
 import ProductGrid from '../components/ProductGrid'
-import staticData from '../../config/defaults/static-products'
+import searchResults from '../../config/defaults/search-results'
 
-const mock = new MockAdapter(axios);
 const defaultFilters = [
   {
-    field:'productType',
-    label:'Product Type'
-  }
+    field: 'productType',
+    label: 'Product Type',
+  },
 ]
 
 storiesOf('Components | Search', module)
@@ -28,93 +27,91 @@ storiesOf('Components | Search', module)
   .addDecorator(withKnobs)
   .addDecorator(StoryRouter())
   .addDecorator(withActions())
-  .addDecorator(() => {
-    return {
-      template: `
-        <div style="max-width: 1200px; margin: 3rem auto;"><story/></div>
-      `
-    }
-  })
+  .addDecorator(() => ({
+    template: `
+      <div style="max-width: 1200px; margin: 3rem auto;"><story/></div>
+    `,
+  }))
   .add(
     'Composition',
-    () => {
-      mock.onGet('/data/shop/static.json').reply(200, staticData)
+    () => ({
+      store,
+      components: {
+        SearchBox,
+        RefinementFilters,
+        SearchResults,
+        SearchNoResults,
+        ProductGrid,
+      },
+      props: {
+        filters: {
+          type: Array,
+          default: object('Filters Object', defaultFilters),
+        },
+      },
+      data() {
+        return {
+          filteredData: [],
+        }
+      },
+      computed: {
+        products() {
+          return this.$store.getters['search/productData']
+        },
+        query() {
+          return this.$store.state.search.query
+        },
+      },
+      created() {
+        const mock = new MockAdapter(axios)
 
-      return {
-        store,
-        components: {
-          SearchBox,
-          RefinementFilters,
-          SearchResults,
-          SearchNoResults,
-          ProductGrid
+        mock.onGet('/data/search.json').reply(200, searchResults)
+
+        this.$store.dispatch('search/getProductData')
+      },
+      methods: {
+        onUpdate(outputData) {
+          this.filteredData = outputData
         },
-        props: {
-          filters: {
-            type: Array,
-            default: object('Filters Object', defaultFilters)
-          }
-        },
-        data() {
-          return {
-            filteredData: []
-          }
-        },
-        computed: {
-          products () {
-            return this.$store.getters['search/productData']
-          },
-          query () {
-            return this.$store.state.search.query
-          }
-        },
-        created () {
-          this.$store.dispatch('search/getProductData')
-        },
-        methods: {
-          onUpdate (outputData) {
-            this.filteredData = outputData
-          }
-        },
-        template: `
-          <div>
-            <div class="columns is-centered">
-              <div class="column is-3">
-                <search-box position="in-page" />
-              </div>
-            </div>
-            <div class="columns">
-              <div class="column is-3">
-                <refinement-filters
-                  :input-data="products"
-                  :filter-properties="filters"
-                  v-on:updated="onUpdate"
-                />
-              </div>
-              <div class="column is-9">
-                <search-results
-                  v-if="filteredData"
-                  :searchData="filteredData"
-                  :searchQuery="query"
-                >
-                  <template v-slot:result="{ result }">
-                    <product-grid :products="result" :columns="2" />
-                  </template>
-                  <template v-slot:no-results>
-                    <search-no-results />
-                  </template>
-                </search-results>
-              </div>
+      },
+      template: `
+        <div>
+          <div class="columns is-centered">
+            <div class="column is-3">
+              <search-box position="in-page" />
             </div>
           </div>
-        `
-      }
-    },
+          <div class="columns">
+            <div class="column is-3">
+              <refinement-filters
+                :input-data="products"
+                :filter-properties="filters"
+                v-on:updated="onUpdate"
+              />
+            </div>
+            <div class="column is-9">
+              <search-results
+                v-if="filteredData"
+                :searchData="filteredData"
+                :searchQuery="query"
+              >
+                <template v-slot:result="{ result }">
+                  <product-grid :products="result" :columns="2" />
+                </template>
+                <template v-slot:no-results>
+                  <search-no-results />
+                </template>
+              </search-results>
+            </div>
+          </div>
+        </div>
+      `,
+    }),
     {
       info: {
         summary: `
         This is a complete example of all search components.
-        `
-      }
+        `,
+      },
     }
   )
