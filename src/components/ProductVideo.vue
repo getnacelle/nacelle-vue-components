393dbc23f5502fa98c861aec65569d7960068927
Nick Richmond <nwrichmond@turnt.tech>
2019-11-26 15:53:26 -0500

    Add semantic HTML5 <source> with video format type
    This will make it easier to include multiple formats for browser support

--- src/components/ProductVideo.vue
+++ src/components/ProductVideo.vue
@@ -2,12 +2,17 @@
   <div class="product-video nacelle">
     <video
       ref="product-video"
-      :src="source"
       :controls="showControls"
       :loop="loop"
       :muted="muted"
       :autoplay="autoplay"
-    />
+    >
+      <source v-if="formatMp4" :src="source" type="video/mp4" />
+      <source v-if="formatWebm" :src="source" type="video/webm" />
+      <source v-if="formatOgg" :src="source" type="video/ogg" />
+      <source v-if="formatAvi" :src="source" type="video/avi" />
+      <p>Your browser doesn't support the HTML5 video tag.</p>
+    </video>
   </div>
 </template>
 
@@ -37,10 +42,27 @@ export default {
       } else {
         return false
       }
+    },
+    sourceExtension() {
+      return this.source
+        .split('?')[0]
+        .split('.')
+        .reverse()[0]
+    },
+    formatMp4() {
+      return this.sourceExtension === 'mp4'
+    },
+    formatWebm() {
+      return this.sourceExtension === 'webm'
+    },
+    formatOgg() {
+      return this.sourceExtension === 'ogg'
+    },
+    formatAvi() {
+      return this.sourceExtension === 'avi'
     }
   }
 }
 </script>
 
-<style>
-</style>
+<style></style>
