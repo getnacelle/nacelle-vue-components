847b0c44038f73b926ca193d3a4f03f9e61621be
Nick Richmond <nwrichmond@turnt.tech>
2019-11-27 15:35:30 -0500

    Refactor to shorten parameters in optimizeSource()
    Moving containerRef from a fn param to a prop promotes declarative style when calling optimizeSource method

---
+++ dist/mixins/imageOptimize.js
@@ -0,0 +1,184 @@
+export default {
+  props: {
+    containerRef: {
+      // The ref assigned to the image's containing element
+      type: String,
+      default: ''
+    },
+    resize: {
+      type: Boolean,
+      default: true
+    },
+    reformat: {
+      type: Boolean,
+      default: true
+    },
+    cropDirection: {
+      type: String,
+      default: 'center'
+    },
+    blurUp: {
+      type: Boolean,
+      default: true
+    },
+    byDominantColor: {
+      type: Boolean,
+      default: false
+    }
+  },
+  data() {
+    return {
+      blurred: null,
+      containerWidth: null,
+      containerHeight: null,
+      containerPosition: null,
+      newUrl: null
+    }
+  },
+  methods: {
+    optimizeSource({ url = null, format = 'webp' } = {}) {
+      if (typeof url === 'string' && this.containerRef) {
+        if (this.fromShopifyCDN({ url })) {
+          if (this.resize && this.reformat) {
+            if (this.newUrl !== null) {
+              this.newUrl = this.shopifyReformat({
+                src: this.shopifyResize({ src: url }),
+                format
+              })
+            } else {
+              if (this.blurUp) {
+                this.blurred = this.getBlurred({ src: url })
+                this.newUrl = this.blurred
+              } else if (this.byDominantColor) {
+                this.newUrl = this.getDominantColor({ src: url })
+              } else {
+                this.newUrl = ''
+              }
+            }
+          } else if (this.resize && !this.reformat) {
+            if (this.newUrl !== null) {
+              this.newUrl = this.shopifyResize({ src: url })
+            } else if (this.blurUp) {
+              this.blurred = this.getBlurred({ src: url })
+              this.newUrl = this.blurred
+            } else if (this.byDominantColor) {
+              this.newUrl = this.getDominantColor({ src: url })
+            } else {
+              this.newUrl = ''
+            }
+          } else if (!this.resize && this.reformat) {
+            this.newUrl = this.shopifyReformat({ src: url, format })
+          } else {
+            this.newUrl = url
+          }
+        } else this.newUrl = url
+        return this.newUrl
+      }
+      return url
+    },
+    calculateContainer() {
+      if ((process.client || process.browser) && this.containerRef) {
+        this.containerHeight = this.$refs[this.containerRef].clientHeight
+        this.containerWidth = this.$refs[this.containerRef].clientWidth
+        this.containerPosition = window.getComputedStyle(
+          this.$refs[this.containerRef]
+        ).position
+      }
+    },
+    getBlurred({ src = null } = {}) {
+      return this.shopifyResize({
+        src,
+        width: 20,
+        height: ''
+      })
+    },
+    getDominantColor({ src = null } = {}) {
+      return this.shopifyResize({
+        src,
+        width: 1,
+        height: ''
+      })
+    },
+    shopifyResize({ src = null, width = 'auto', height = 'auto' } = {}) {
+      // Request size which closely matches the width of the bounding element,
+      // unless the parent container uses absolute positioning.
+      // Round up size to the nearest 50px increment.
+      function roundedUpToNearest50px(x) {
+        // Return a blank string if less than 50px
+        if (x >= 50) {
+          return +x + 49 - ((+x + 49) % 50)
+        }
+        return ''
+      }
+      if (typeof src === 'string') {
+        const [baseWithExt, args] = src.split('?')
+        const [extension] = Array.from(baseWithExt.split('.')).reverse()
+        const [base] = baseWithExt.split(`.${extension}`)
+        const newWidth =
+          width === 'auto' ? roundedUpToNearest50px(this.containerWidth) : width
+        const newHeight =
+          height === 'auto'
+            ? roundedUpToNearest50px(this.containerHeight)
+            : height
+        const newSizeString =
+          newWidth !== '' || newHeight !== '' ? `_${newWidth}x${newHeight}` : ''
+        const cropString =
+          this.containerPosition === 'absolute'
+            ? `_crop_${this.cropDirection}`
+            : ''
+        const newBase = base.concat(newSizeString, cropString)
+        const newArgs = args
+          ? args.split('&').filter(el => el.includes('width=') === false)
+          : null
+        const newSrc = newBase.concat(`.${extension}?${newArgs}`)
+        return newSrc
+      }
+      return null
+    },
+    shopifyReformat({ src = null, format = 'webp' } = {}) {
+      // Takes either a png or jpg (other formats will not work),
+      // Returns query string for image in WebP or PJPG format.
+      // NOTE 1: Transformation only works on png and jpg images.
+      // NOTE 2: This function defaults to webp (preferred by Lighthouse)
+      //
+      // Example 1:
+      //  shopifyReformat({ src: "https://cdn.shopify.com/s/files/myPicture.png", format: 'pjpg'})
+      //  returns: "https://cdn.shopify.com/s/files/myPicture.png&format=pjpg"
+      // Example 2:
+      //  shopifyReformat({ src: "https://cdn.shopify.com/s/files/myPicture.jpg", format: 'webp'})
+      //  returns: "https://cdn.shopify.com/s/files/myPicture.jpg&format=webp"
+      //
+      if (typeof src === 'string') {
+        const extension = Array.from(src.split('?v=')[0].split('.')).pop()
+        if (extension === 'png' || extension === 'jpg') {
+          return src.split('&format=')[0].concat(`&format=${format}`)
+        } else {
+          // return the original image if it is a gif / not a png or jpg
+          return src
+        }
+      } else {
+        return null
+      }
+    },
+    fromShopifyCDN({ url = null } = {}) {
+      if (typeof url === 'string') {
+        return url.split('.com')[0] === 'https://cdn.shopify'
+      }
+      return false
+    },
+    fromMagentoCDN({ url = null } = {}) {
+      // Note that not all Magento stores use images from the Magento CDN
+      if (typeof url === 'string') {
+        const [, str1, str2, str3] = url.split('://')[1].split('/')
+        return str1.concat('/', str2, '/', str3) === 'media/catalog/product'
+      }
+      return false
+    }
+  },
+  mounted() {
+    this.calculateContainer()
+  },
+  updated() {
+    this.calculateContainer()
+  }
+}
